adc64c103ea4a6915591660585825cac23da9954
soufiane chaghil <soufianechaghil@gmail.com>
2022-05-17 17:36:53 +0100

    Seller can get orders

---
+++ controllers/seller/getOrders.js
@@ -0,0 +1,32 @@
+const getUser = require("../../models/user/getUser");
+const orderSchema = require('../../models/order')
+
+const getOrders = async (req, res) => {
+  const { username } = req.user;
+
+  try {
+    // get the user
+    const userResult = await getUser(username);
+    if (!userResult.done) throw { message: userResult.message };
+
+    // check if the user exist
+    if (!userResult.user) throw { message: "User does not exist", status: 401 };
+
+    const { userType, _id } = userResult?.user;
+    // check if the user is a seller
+    if (userType !== "seller")
+      throw { message: "A buyer can not get the orders", status: 401 };
+
+    // get the orders
+    const orders = await orderSchema.find({seller: _id}).populate({path: "products"}).populate({path: "buyer"})
+    if(orders.length === 0) res.status(200).json({message: "You have no orders"})
+
+    res.status(200).json({orders})
+  } catch (e) {
+    res
+      .status(e.status || 400)
+      .json({ message: e.message || "Something went wrong" });
+  }
+};
+
+module.exports = getOrders;
--- controllers/seller/index.js
+++ controllers/seller/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { Verify } = require('../verifyJwt')
 const createCatalog = require('./createCatalog')
+const getOrders = require('./getOrders')
 
 const sellerRouter = express.Router()
 
 sellerRouter.post('/create-catalog', Verify, createCatalog)
+sellerRouter.get('/orders', Verify, getOrders)
 
 module.exports = sellerRouter
